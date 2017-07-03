e44f15fcc953e07d8fd81efff21c8a5818b904de
Tim <tim@inexpro.nl>
2017-07-03 21:25:52 +0200

    Set use urls correct

--- Program.cs
+++ Program.cs
@@ -9,8 +9,9 @@ namespace WorkCounter
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-				.UseUrls("http://192.168.1.172:5006")
-                .UseKestrel()
+				//.UseUrls("http://192.168.1.172:5006")
+				.UseUrls("http://localhost:5006")
+				.UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
