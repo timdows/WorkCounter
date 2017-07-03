dafec5d89f2db30d874bb6c7205ad3e33c530ba4
Tim <tim@inexpro.nl>
2017-07-03 19:18:00 +0200

    Create webapi project and commit arduino code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace WorkCounter
+{
+	public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+				.UseUrls("http://localhost:5006")
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
